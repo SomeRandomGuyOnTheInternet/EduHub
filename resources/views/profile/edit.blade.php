ff19fe9f5c8393feb76fe5ff75b3fc4b7ba9b940
Vignesh <bala12rupesh@gmail.com>
2024-07-19 04:41:54 +0800

    Update edit.blade.php

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -2,7 +2,7 @@
     @livewire('professor.sidebar', ['currentPage' => ProfessorSidebarLink::Profile])
 
     <div class="container-fluid p-0">
-        @livewire('professor.module-header', ['currentPage' => "Profile", 'currentModuleId' => $module_id])
+        @livewire('professor.module-header', ['currentPage' => "Profile"])
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
             <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
                 <div class="max-w-xl">
